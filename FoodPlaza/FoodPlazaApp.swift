6712727ec1d2f438eedbf77aa9bc3ab3a1554a04
Lalit Rao <lalitkumaryadav9090@gmail.com>
2024-10-22 17:57:36 +0530

    Initial Commit

---
+++ FoodPlaza/FoodPlazaApp.swift
@@ -0,0 +1,17 @@
+//
+//  FoodPlazaApp.swift
+//  FoodPlaza
+//
+//  Created by Lalit Kumar on 22/10/24.
+//
+
+import SwiftUI
+
+@main
+struct FoodPlazaApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ FoodPlazaTests/FoodPlazaTests.swift
@@ -0,0 +1,17 @@
+//
+//  FoodPlazaTests.swift
+//  FoodPlazaTests
+//
+//  Created by Lalit Kumar on 22/10/24.
+//
+
+import Testing
+@testable import FoodPlaza
+
+struct FoodPlazaTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
